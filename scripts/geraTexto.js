6bd2d3d7acb46c1b27447d9085b017d2d6f45e8b
Roberto Murilo Martins Cordeiro <robertomartins@users.noreply.github.com>
2021-01-04 14:59:09 -0300

    Corrigindo links dos diretorios.

--- scripts/geraTexto.js
+++ scripts/geraTexto.js
@@ -9,7 +9,7 @@ var sexo = localStorage.getItem("sexo");
 function montarTexto(){
     var conteudo = document.getElementById("concluido");
 
-    conteudo.innerHTML="<h2>Cadastro efetuado com sucesso!</h2><img src='img/concluido.jpg'>"+
+    conteudo.innerHTML="<h2>Cadastro efetuado com sucesso!</h2><img src='images/concluido.jpg'>"+
     "<p>Parabens <strong>"+nome+"</strong> seu cadastro foi efetuado com sucesso.</p>"+
     "<p>Fique atento no seu email <strong>"+email+"</strong>, você irá receber dicas de treino "+
     "especificas para quem é do sexo <strong>"+sexo+"</strong> e leva uma vida <strong>"+atividade+"</strong>.</p>";
